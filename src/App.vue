4d76187b2074bcd23203eff55b37e518bf14bda0
vfuster66 <vfuster-@student.42perpignan.fr>
2024-11-15 22:13:57 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-blue-500 text-white">
+    <header class="text-center mb-4">
+      <h1 class="text-4xl font-bold">Vérification de Tailwind CSS</h1>
+      <p class="text-lg mt-2">Si ce texte est blanc et le fond est bleu, Tailwind fonctionne !</p>
+    </header>
+
+    <!-- Contenu principal des routes -->
+    <main class="w-full p-4">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+/* Aucun style personnalisé n'est requis pour ce test */
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env,
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
+});
