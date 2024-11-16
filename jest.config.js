2141e4a4c161c5e3cf69b6fec0bc0cd297ec452c
vfuster66 <vfuster-@student.42perpignan.fr>
2024-11-16 18:47:52 +0100

    Modification appel a test dans package.json

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 export default {
   transform: {
-    "^.+\\.js$": "babel-jest"
+    "^.+\\.js$": [
+      "babel-jest",
+      { configFile: "./babel.config.js" }
+    ],
   },
-  testEnvironment: "node"
+  testEnvironment: "node",
 };
